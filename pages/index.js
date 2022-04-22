8e41a7d35bbf48ae7a351e66fa4529a3346d1745
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-04-22 07:05:06 +0200

    always

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Example() {
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <div className="block xl:inline">نظام ترقيم&reg; لإدارة المختبرات الطبية</div>
               
-              <div className="block text-indigo-600 xs-mt-3 mt-6">مُختبرك دائماً بين يديك</div>
+              <div className="block text-indigo-600 xs-mt-3 mt-6">مُختبرك دائمًا بين يديك</div>
             </h1>
             <p className="mt-6 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-10 md:text-xl md:max-w-3xl">
               منظومة متكاملة من الأدوات والتطبيقات تساعدك على الوصول للبيانات بسهولة ويسر وكذلك التحكم في جميع جوانب استثمارك من الجانب الإداري والتقني
