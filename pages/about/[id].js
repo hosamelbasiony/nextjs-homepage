51aa95c00e31797a5b07b358213779c07d462a6f
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-04-22 06:16:23 +0200

    init

---
+++ pages/about/[id].js
@@ -0,0 +1,112 @@
+import Layout from "../../components/layout";
+
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/typography'),
+    ],
+  }
+  ```
+*/
+export default function Example() {
+    return (
+        <Layout>
+            <div className="py-2 mt-12 px-4 sm:px-6 lg:px-8 overflow-hidden" style={{ direction: 'rtl' }}>
+                <div className="max-w-max lg:max-w-7xl mx-auto">
+                <div className="relative z-10 mb-8 md:mb-2 md:px-6">
+                    <div className="text-base max-w-prose lg:max-w-none">
+                    <h2 className="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">ترقيم&trade; لإدارة المختبرات</h2>
+                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        الطريقة المثلى لإدارة مختبرك
+                    </p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <svg
+                    className="hidden md:block absolute top-0 right-0 -mt-20 -mr-20"
+                    width={404}
+                    height={384}
+                    fill="none"
+                    viewBox="0 0 404 384"
+                    aria-hidden="true"
+                    >
+                    <defs>
+                        <pattern
+                        id="95e8f2de-6d30-4b7e-8159-f791729db21b"
+                        x={0}
+                        y={0}
+                        width={20}
+                        height={20}
+                        patternUnits="userSpaceOnUse"
+                        >
+                        <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width={404} height={384} fill="url(#95e8f2de-6d30-4b7e-8159-f791729db21b)" />
+                    </svg>
+                    <svg
+                    className="hidden md:block absolute bottom-0 left-0 -mb-20 -ml-20"
+                    width={404}
+                    height={384}
+                    fill="none"
+                    viewBox="0 0 404 384"
+                    aria-hidden="true"
+                    >
+                    <defs>
+                        <pattern
+                        id="7a00fe67-0343-4a3c-8e81-c145097a3ce0"
+                        x={0}
+                        y={0}
+                        width={20}
+                        height={20}
+                        patternUnits="userSpaceOnUse"
+                        >
+                        <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width={404} height={384} fill="url(#7a00fe67-0343-4a3c-8e81-c145097a3ce0)" />
+                    </svg>
+                    <div className="relative md:p-6">
+                    <div className="lg:grid lg:grid-cols-2 lg:gap-6">
+                        <div className="prose prose-indigo prose-lg text-gray-500 lg:max-w-none">
+                        <p>
+                        نالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. بينما تعمل جميع مولّدات نصوص لوريم إيبسوم على الإنترنت على إعادة تكرار مقاطع من نص لوريم إيبسوم نفسه عدة مرات بما تتطلبه الحاجة
+                        </p>
+                        <p>
+                        خلافاَ للإعتقاد السائد فإن لوريم إيبسوم ليس نصاَ عشوائياً، بل إن له جذور في الأدب اللاتيني الكلاسيكي منذ العام 45 قبل الميلاد، مما يجعله أكثر من 2000 عام في القدم. قام البروفيسور "ريتشارد ماك لينتوك" (Richard McClintock) وهو بروفيسور اللغة اللاتينية في جامعة هامبدن-سيدني في فيرجينيا بالبحث عن أصول كلمة لاتينية غامضة في نص لوريم إيبسوم وهي "consectetur"، وخلال تتبعه لهذه الكلمة في الأدب اللاتيني اكتشف المصدر الغير قابل للشك. فلقد اتضح أن كلمات نص لوريم إيبسوم تأتي من الأقسام 1.10.32 و 1.10.33 من كتاب "حول أقاصي الخير والشر"
+                        </p>
+                        
+                        </div>
+                        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 lg:mt-0">
+                        <p>
+                        هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.
+                        </p>
+                        <p>
+                        هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. بينما تعمل جميع مولّدات نصوص لوريم إيبسوم على الإنترنت على إعادة تكرار مقاطع من نص لوريم إيبسوم نفسه عدة مرات بما تتطلبه الحاجة، يقوم مولّدنا هذا باستخدام كلمات من قاموس يحوي على أكثر من 200 كلمة لا تينية، مضاف إليها مجموعة من الجمل النموذجية، لتكوين نص لوريم إيبسوم ذو شكل منطقي قريب إلى النص الحقيقي.
+                        </p>
+                        </div>
+                    </div>
+                    <div className="mt-8 inline-flex rounded-md shadow">
+                        <a
+                        href="#"
+                        className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                        >
+                        تواصل معنا
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </Layout>
+    )
+  }
+  
---
+++ pages/about/index.js
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import { CheckIcon } from '@heroicons/react/outline'
+
+import Layout from '../../components/layout'
+
+const features = [
+  {
+    name: 'Core LIS System',
+    description: 'Core modules managing registeration, result reports management, sampling, financial aspects, host interfacing and more.',
+  },
+  {
+    name: 'Patient Results Portal',
+    description: 'Easy to use patient results portal for results viewing and patient feedback.',
+  },
+  {
+    name: 'Lab to Lab Results Portal',
+    description: 'Elegant lab to lab results portal for lab to lab results viewing printout and pdf reports generation.',
+  },
+  {
+    name: 'Integrated ERP system',
+    description: 'The LIS system is in the heart of an integrated ERP system. The ERP system is used for financial aspects, inventory management, and more.',
+  }
+]
+
+export default function Example() {
+  return (
+    <Layout>
+        <div>
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+                <div className="max-w-3xl mx-auto text-center">
+                <h2 className="text-3xl font-extrabold text-gray-900">All-in-one platform</h2>
+                <p className="mt-4 text-lg text-gray-500">
+                    Tarqeem LIS is a complete solution for managing your laboratory.
+                </p>
+                </div>
+                <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-2 lg:gap-x-8">
+                {features.map((feature) => (
+                    <Link href='/about/1' key={feature.name}>
+                      <a  className="relative">
+                        <dt>
+                            <CheckIcon className="absolute h-6 w-6 text-green-500" aria-hidden="true" />
+                            <p className="ml-9 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                        </dt>
+                        <dd className="mt-2 ml-9 text-base text-gray-500">{feature.description}</dd>
+                      </a>
+                    </Link>
+                ))}
+                </dl>
+            </div>
+        </div>
+    </Layout>
+  )
+}
